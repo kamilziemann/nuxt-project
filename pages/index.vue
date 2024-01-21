13b27dd30bcc3fc2583e3c6ddccfa554d9d3d300
Kamil Ziemann <kamilziemann1@gmail.com>
2024-01-21 03:08:09 +0100

    Added grid for top products

--- pages/index.vue
+++ pages/index.vue
@@ -13,9 +13,12 @@ const topProductsData = computed(() => products.filter((product) => topProducts.
 	<PageHeader />
 	<div class="container mx-auto px-4 pt-7 pb-80">
 		<h2 class="text-center text-5xl font-semibold mt-5 mb-9">Top Oferty</h2>
-		<div class="flex flex-row gap-3 items-center justify-center">
+
+		<div
+			class="grid grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))] gap-3 items-center justify-center"
+		>
 			<Card
-			type="buy"
+				type="buy"
 				v-for="product in topProductsData"
 				:key="product.id"
 				:id="product.id"
@@ -25,6 +28,7 @@ const topProductsData = computed(() => products.filter((product) => topProducts.
 				:price="product.price"
 			/>
 		</div>
+
 		<div class="flex justify-center mt-5">
 			<NuxtLink
 				className="link"
